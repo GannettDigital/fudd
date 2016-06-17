6eb2492dfe3fa2ec25179b239ebd82f769cdb2cf
jhaugh42@gmail.com <jhaugh42@gmail.com>
2016-06-17 17:34:09 -0400

    initial commit

---
+++ test/unit/fudd.js
@@ -0,0 +1,18 @@
+'use strict';
+var expect = require('chai').expect;
+
+describe('fudd', function() {
+
+    var fudd;
+    before(function () {
+        var Fudd = require('../../lib/fudd.js');
+        fudd = new Fudd();
+    });
+
+    describe('init', function() {
+        it('should return true', function() {
+            var result = fudd.init();
+            expect(result).to.equal(true);
+        });
+    });
+});
