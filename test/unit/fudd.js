32a7ce4eb9ccb244878ba45c6db92107239941a8
jhaugh42@gmail.com <jhaugh42@gmail.com>
2016-06-20 13:59:39 -0400

    fixed some sequencing logic

--- test/unit/fudd.js
+++ test/unit/fudd.js
@@ -5,12 +5,14 @@ var sinon = require('sinon');
 
 describe('fudd', function() {
 
-    var fudd;
+    var Fudd;
     var configUtilsStub;
 
     before('enable mockery', function() {
         mockery.enable({useCleanCache: true});
     });
+
+    after('disable mockery', mockery.disable);
 
     beforeEach('setup mocks', function(){
         mockery.registerAllowable('../../lib/fudd.js');
@@ -20,26 +22,17 @@ describe('fudd', function() {
         mockery.registerMock('palinode', {});
         mockery.registerMock('./amqp-config-utils.js', configUtilsStub = sinon.stub().returns('amqp://some.host'));
 
-        fudd = require('../../lib/fudd.js');
+        Fudd = require('../../lib/fudd.js');
     });
 
-    after('disable mockery', mockery.disable);
-
     describe('setup', function() {
-        it('should call connect with supplied config', function() {
-            var connectSpy = sinon.spy();
-            fudd.__proto__._connect = connectSpy;
-            var config = {key: 'value'};
-            fudd.setup(config);
-            expect(connectSpy.calledWith(config)).to.equal(true);
-        });
+        var config = {thisIs: 'config'};
+        var connectBindStub;
 
-        it('should call connect with supplied config', function() {
-            var connectSpy = sinon.spy();
-            fudd.__proto__._connect = connectSpy;
-            var config = {key: 'value'};
-            fudd.setup(config);
-            expect(connectSpy.calledWith(config)).to.equal(true);
-        });
+        before('setup stubs', function() {
+            connectBindStub
+        })
+
+
     });
 });
